9eb98933ab7863ee50f56141a7ac35b246d1095f
alex <c.alex.pellett@gmail.com>
2018-12-20 21:21:37 -0500

    doc

---
+++ R/QCEWdata-package.R
@@ -0,0 +1,8 @@
+#' United States Quarterly Census of Employment and Wages Data
+#'
+#' Monthly employment and wages by industrial sector and county.
+#'
+#' @docType package
+#' @name QCEWdata
+#'
+NULL
